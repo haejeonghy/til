76804e8c458899228ccaafc299fb30d5020365b5
hae jeong yu <haejeonghy@gmail.com>
2022-06-07 16:14:08 +0900

    Feat: Web3.js 사용하여 가나슈에 배포한 컨트랙트 실행 함수 추가

--- blockchain-development/code/beb-sprint-web3js/index.js
+++ blockchain-development/code/beb-sprint-web3js/index.js
@@ -2,6 +2,7 @@ const express =require('express');
 const app = express();
 const port = 8080;
 const Web3 = require('web3');
+const Contract = require('web3-eth-contract');
 
 function getWeb3() { 
     const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
@@ -18,6 +19,42 @@ async function getAccounts() {
         return e;
     }
 }
+
+async function helloWorld() {
+    try {
+        const abi = 
+            [
+                {
+                    "inputs": [],
+                    "name": "renderHelloWorld",
+                    "outputs": [
+                        {
+                            "internalType": "string",
+                            "name": "greeting",
+                            "type": "string"
+                        }
+                    ],
+                    "stateMutability": "pure",
+                    "type": "function"
+                }
+            ];
+        const address = '0xa2A75B428631704600BA8de9d815f5Bc672EB825';
+        Contract.setProvider('http://0.0.0.0:7545');
+        const contract = new Contract(abi, address);
+        const result = await contract.methods.renderHelloWorld().call();
+        console.log(result);
+        return result;
+    } catch(e) {
+        console.log(e);
+        return e;
+    }
+}
+
+app.get('/helloworld', (req, res) => {
+    helloWorld().then((result) => {
+        res.send(result);
+    })
+})
 
 app.get('/', (req, res) => {
     getAccounts().then((accounts) => {
